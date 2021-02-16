98de47432e3389cb7e8e9cb6ad525b5230a74055
David Jun <djun301@gmail.com>
2021-02-16 03:29:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,10 +1,18 @@
 # require modules here
-
-def load_library
-  # code goes here
+require 'pry' 
+require "yaml"
+def load_library(path)
+f_hash = {}
+YAML.load_file(path).each do |x, y|
+  f_hash[x] = {}
+  f_hash[x][:english] = y[0]
+  f_hash[x][:japanese] = y[1]
+end
+f_hash  # code goes here
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(path, emoticon)
+  
   # code goes here
 end
 
