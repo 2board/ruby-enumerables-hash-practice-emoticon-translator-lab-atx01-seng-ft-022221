acd922266e6a044d0de0d5b0727c39c383faf532
David Jun <djun301@gmail.com>
2021-02-16 04:19:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,7 @@ end
 
 def get_japanese_emoticon(path, emoticon)
   load_library(path).each do |key, value|
-    if value[:english] == emoticon
+    if value[:japanese] == emoticon
       return key
   # code goes here
 end
